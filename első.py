7d11d8278baef766b2d16c410f3bb7ce1644f888
valamiis <spisakr@kkszki.hu>
2022-09-06 14:34:34 +0200

    v1.02

--- első.py
+++ első.py
@@ -1,4 +1,1 @@
-print("Mondd el, miért, miért van, hogy fáj?\
-Miért van, hogy bárhová megyek\
-Az éj leple alatt újra és újra rámtalálsz?\
-Miért is félsz?")
+print("Mondd el, miért, miért van, hogy fáj?\nMiért van, hogy bárhová megyek\nAz éj leple alatt újra és újra rámtalálsz?\nMiért is félsz?")
